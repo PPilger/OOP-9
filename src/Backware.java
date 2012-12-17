cc1432fb5824e0b2839ca7b6b4084046b17df96f
kletzi <kletzi@192.168.0.11>
2012-12-17 21:37:10 +0100

    Zusicherungen: *) Backware *) Bestellung

--- src/Backware.java
+++ src/Backware.java
@@ -1,4 +1,6 @@
-
+/**
+ * Ein Keks und ein Doppelkeks sind eine Backware.
+ */
 public interface Backware {
 
 }
--- src/Bestellung.java
+++ src/Bestellung.java
@@ -1,6 +1,10 @@
 import java.util.Iterator;
 import java.util.LinkedList;
 
+/**
+ * Eine Bestellung kann mehrere Positionen aufnehmen und speichern.
+ * Nach einer Ansammlung von Positionen können diese gebacken werden.
+ */
 public class Bestellung {
 	private LinkedList<Position> postenListe;
 
@@ -8,10 +12,21 @@ public class Bestellung {
 		this.postenListe = new LinkedList<Position>();
 	}
 
+	/**
+	 * Fügt eine Position in die Liste hinzu.
+	 * 
+	 * @param p
+	 * 		Der Parameter muss eine Position sein.
+	 * 
+	 * Vorbedingung: p darf nicht null sein.
+	 */
 	public void add(Position p) {
 		this.postenListe.add(p);
 	}
 
+	/**
+	 * Gibt die Bestellliste mit all ihren Positionen aus.
+	 */
 	public void drucke() {
 		Iterator<Position> it = this.postenListe.iterator();
 		int num = 1;
@@ -24,6 +39,13 @@ public class Bestellung {
 		}
 	}
 
+	/**
+	 * Hier werden alle Positionen der Bestellung ausgebacken und in eine Dose gelegt.
+	 * @return
+	 * 		Gibt eine gültige Instanz eines Dose-Objekts, gefüllt mit Keksen zurück.
+	 * 
+	 * Vorbedingung: Die postenListe darf nicht null sein.
+	 */
 	public Dose backe() {
 		Dose keksdose = new Dose();
 
